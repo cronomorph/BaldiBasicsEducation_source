d3a46de3d88d54d26daa7bc7003e0888ccd64c86
Keegan Stanislav <ScottLoganMaxwell@gmail.com>
2024-12-19 11:36:47 -0500

    Update PickupAnimationScript.cs

--- Assembly-CSharp/Assembly-CSharp/PickupAnimationScript.cs
+++ Assembly-CSharp/Assembly-CSharp/PickupAnimationScript.cs
@@ -1,17 +1,71 @@
-// PickupAnimationScript
+// Credit to YuraSuper2048
+using System;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class PickupAnimationScript : MonoBehaviour
 {
-	private Transform itemPosition;
+    // Change this to control frequency of animation
+    // Less = more optimized, but will increase animation delays
+    // More = less optimized, will reduce animation delays
+    // (This actually is distance to player where update will take 1 second)
+    private const float UpdateFrequency =
+        400;
+    
+    // Time before last update
+    private float time;
 
-	private void Start()
-	{
-		itemPosition = base.GetComponent<Transform>();
-	}
+    // Time to wait before billboarding
+    private float waitTime;
+    
+    // Amplitude of animation
+    [SerializeField]
+    private float amplitude = 0.5f;
+    
+    // Center Y
+    [SerializeField]
+    private float centerY = 1f;
+    
+    // Needed to reduce the built-in unity calls
+    private new Transform transform;
 
-	private void Update()
-	{
-		itemPosition.localPosition = new Vector3(0f, Mathf.Sin((float)Time.frameCount * 0.0174532924f) / 2f + 1f, 0f);
-	}
+    private void Start()
+    {
+        // Getting transform
+        transform = base.transform;
+    }
+
+    private void OnWillRenderObject()
+    {
+        // Getting current camera for further use
+        var cameraCurrent = Camera.current;
+        
+        // Counting time
+        time += Time.deltaTime;
+        
+        // Waiting some time based on distance
+        if (time < waitTime) return;
+        time = 0;
+
+        try
+        {
+            // If distance is too high, return
+            var distance = Vector3.Distance(transform.position, cameraCurrent.transform.position);
+            if (distance > 125)
+            {
+                return;
+            }
+            
+            // Calcualting time to wait
+            waitTime = distance / UpdateFrequency;
+        }
+        catch
+        {
+            // Ignore all errors
+        }
+
+        // Animate
+        transform.localPosition = Vector3.up *
+                                      (MathF.Sin(Time.time * 2) * amplitude + centerY);
+    }
 }
